11422ae678f944d6608c7c54cc49c29146c47339
mashabburanov <mashrabburanovgmail.com>
2024-05-12 15:08:31 +0600

    Initial Commit

---
+++ SCHEDULE/SCHEDULEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCHEDULEApp.swift
+//  SCHEDULE
+//
+//  Created by Djanbolotov Askabek on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SCHEDULEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
